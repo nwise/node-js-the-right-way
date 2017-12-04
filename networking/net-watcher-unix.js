e0714429ba1e611531868b9ca4ae249b8df9ff87
Nathan Wise <nathanwise@gmail.com>
2017-12-03 22:03:09 -0500

    Initial Networking Examples

---
+++ networking/net-watcher-unix.js
@@ -0,0 +1,18 @@
+'use strict';
+const fs = require('fs');
+const net = require('net');
+const filename = process.argv[2];
+
+if(!filename){
+    throw Error('Error: No filename specified.');
+}
+
+net.createServer(connection => {
+    console.log('Subscriber connected.');
+    console.log(`Now watching "${filename}" for changes...`);
+    const watcher = fs.watch(filename, () => connection.write(`File changed ${new Date()}\n`));
+    connection.on('close', () => {
+        console.log('Subscriber disconnected');
+        watcher.close();
+    });
+}).listen('/tmp/watcher.sock', () => console.log('Listening for subscribers...'));
